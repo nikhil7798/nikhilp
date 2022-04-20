88a6a2757cbb385fb84a1cff98b488c04c5f7629
nikhil7798 <nikhilpawar@gmail.com>
2022-04-20 14:06:52 +0530

    ok

---
+++ src/ParaPass.java
@@ -0,0 +1,16 @@
+
+public class ParaPass 
+{
+  public void m1(int a ,String b)
+  {
+	  System.out.println(a);
+	  System.out.println(b);
+  }
+	public static void main(String[] args)
+	{
+		 ParaPass  a=new  ParaPass();
+		 a.m1(10,"NIKHIL");
+      
+	}
+
+}
---
+++ src/Parapassing.java
@@ -0,0 +1,17 @@
+
+public class Parapassing 
+{
+	public void m1(int a,String b)
+			{
+		     System.out.println(a);
+		     System.out.println(b);
+			}
+	
+	public static void main(String[] args) 
+	{
+	 Parapassing a=new Parapassing();
+	 a.m1(7,"Ganesh");
+
+	}
+
+}
